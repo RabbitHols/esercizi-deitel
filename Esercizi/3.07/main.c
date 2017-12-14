a8def39f54a52de9c5c0e38690e16d17d7a86c0a
RabbitHols <rob.turturica@stud.uniroma3.it>
2017-12-14 01:05:05 +0100

    Add 3.07

---
+++ Esercizi/3.07/main.c
@@ -0,0 +1,34 @@
+/**
+
+Scrivere delle istruzioni che visualizzino le seguenti sequenze di valori:
+
+a) 1,2,3,4,5,6,7
+b) 3,8,13,18,23
+c) 20,14,8,2,-4,-10
+d) 19,27,35,43,51
+
+**/
+
+#include  <stdio.h>
+
+int main()
+{
+	
+	printf("a) ");
+
+	int cont = 0;
+
+	for(cont = 0; cont < 8; cont++){
+		printf("%d ", cont);
+	}
+
+	printf("\n");
+
+	for(cont = 0; cont < 5; cont++){
+		
+	}
+
+
+
+	return 0;
+}
