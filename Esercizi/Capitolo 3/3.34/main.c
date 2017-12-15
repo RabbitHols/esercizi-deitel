630796d09c2639f9ccbfb199a7c5c946e6d16942
Robert Turturica <you@example.com>
2017-12-15 14:44:56 +0100

    Da modificare 3.34

--- Esercizi/Capitolo 3/3.34/main.c
+++ Esercizi/Capitolo 3/3.34/main.c
@@ -11,3 +11,27 @@ Ex:
 
 */
 
+#include <stdio.h>
+
+int main(){
+
+	int lato;
+
+	printf("Dammi lato quadrato: ");
+	scanf("%d", &lato);
+
+	int row, column;
+
+	for(row = 0; row < lato; row++){
+
+		for(column = 0; column < lato; column++){
+			if(row == 0 || row == lato - 1){printf("\t*"); }
+			else {	printf("\t+"); }
+		}
+
+		printf("\n\n\n");
+	}
+
+
+	return 0;
+}
