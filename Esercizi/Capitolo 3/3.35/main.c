a0cb33a657ed32498b7792adf6f3dbdc9171b0c8
Robert Turturica <rob.turturica@stud.uniroma3.it>
2017-12-21 02:58:43 +0100

    Eliminato un file inutile da 3.35

--- Esercizi/Capitolo 3/3.35/main.c
+++ Esercizi/Capitolo 3/3.35/main.c
@@ -2,30 +2,36 @@
 Prendere un numero di 5 cifre e si decida se esso è palindromo 
 */
 
+/*
+ * Un modo per risolvere l'esercizio è creare un numero inverso tramite gli operatori / e % 
+ * e vedere se sono uguali
+ */
+
 #include <stdio.h>
 int main()
 {
-    int n, reversedInteger = 0, remainder, originalInteger;
+	int numero;
+	int palindromo;
+	int revPlindromo;
+	int store;
 
-    printf("Enter an integer: ");
-    // scanf("%d", &n);
-	n = 23232;
-	
-    originalInteger = n;
+	printf("### DAMMI UN NUMERO ###\n");
+	scanf("%d", &numero);
 
-    // reversed integer is stored in variable 
-    while( n!=0 )
-    {
-        remainder = n%10;
-        reversedInteger = reversedInteger*10 + remainder;
-        n /= 10;
-    }
+	palindromo = numero;	
+	revPlindromo = 0;
 
-    // palindrome if orignalInteger and reversedInteger are equal
-    if (originalInteger == reversedInteger)
-        printf("%d is a palindrome.", originalInteger);
-    else
-        printf("%d is not a palindrome.", originalInteger);
-    
+	while(numero != 0){
+		store = numero % 10;
+		revPlindromo = (revPlindromo * 10) + store;
+		//printf("Store :%d\n", store);
+		//intf("RevPal:%d\n", revPlindromo);
+		numero = numero / 10;
+	}	
+  	
+	if(palindromo == revPlindromo){printf("PALINDROMO\n");}
+	else { printf("NON PALINDROMO\n"); };
+
+
     return 0;
 }
