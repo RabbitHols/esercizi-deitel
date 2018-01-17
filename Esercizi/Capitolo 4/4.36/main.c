887a9c1ad9be8188753561960f9da30180327301
Robert Turturica <you@example.com>
2018-01-17 02:40:30 +0100

    Esercizio 4.36 completato

--- Esercizi/Capitolo 4/4.36/main.c
+++ Esercizi/Capitolo 4/4.36/main.c
@@ -8,26 +8,34 @@ compendiato, visualizzato in modo ordinato. Attenti agli anni bisestili
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+typedef enum {false, true} bool;
 
-	int anno;
+int main(int argc, char const *argv[]){
+	if(argv[1] == NULL){
+		return 0;
+	}
+
+	int anno = atoi(argv[1]);
+
 	char mesi[][12] = {"GENNAIO", "FEBBRAIO", "MARZO", "APRILE", "MAGGIO", "GIUGNO", "LUGLIO",
 				  "AGOSTO", "SETTEBRE", "OTTOBRE", "NOVEMBRE", "DICEMBRE"};
 
 	int giorni[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-	printf("### L'anno deve essere compreso tra 1994 - 1999 ### \n");
+	if(anno == 1992 || anno == 1996){
+		giorni[1] = giorni[1] + 1;
+	}
 
-		printf("Che hanno vuoi visualizzare ?\n");
-		// scanf("%d", &anno);
-	
 
 	for(int cnt = 0; cnt < 12; cnt++){
 		printf("%s \n", mesi[cnt]);
 		for(int t = 1; t <= giorni[cnt]; t++){
-			if(t % 5 == 0){printf("\n");};
 			printf("%d\t\t", t);
+			if(t % 7 == 0){
+				printf("\n");
+			}	
 		}
 		printf("\n\n");
 	}
