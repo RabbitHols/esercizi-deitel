744a8e65ed7daed98fdc18c3fbc7003bbfb13889
Robert Turturica <robertturturica@gmail.com>
2018-05-23 17:01:35 +0200

    Scalato il gioco e rifattorizzato alcune funzione.

--- Esercizi/Capitolo 7/7.13/header.h
+++ Esercizi/Capitolo 7/7.13/header.h
@@ -12,26 +12,28 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define NPLAYER 2
-#define ROW     5
-#define CLM     2
+#define NPLAYER 3
+#define ROW 5
+#define CLM 2
 
 //prototipi
 
-bool poker(char *[][2], int[][2], int);
-bool colore(char *[][2], int[][2], int);
-bool tris(char *[][2], int[][2], int);
-bool scala(char *[][2], int[][2], int);
-bool doppiaCoppia(char *[][2], int[][2], int);
-bool coppia(char *[][2], int[][2], int);
+bool doppiaCoppia   (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool poker          (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool colore         (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool tris           (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool scala          (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool coppia         (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool cartaAlta      (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
 
-void shuffle(int[][13]);
-void sortMano(char *[][2], char *[], int[][2]);
-void deal(int (*)[], char *[], char *[]);
-void manoAttuale(int (*)[], char *[], char *[], int[][2]);
-void manoGiocatori(int (*)[], char *[], char *[], int[][2]);
+void shuffle        (int[][13]);
+void deal           (int (*)[], char *[], char *[]);
 
-// Funzioni
+void sortMani       (char * [NPLAYER][ROW][CLM], char *[], int[NPLAYER][ROW][CLM]);
+void mostraMano     (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM]);
+void manoGiocatori  (int (*)[], char *[], char *[], int[][2]);
+void giveCardsCoord (char * [NPLAYER][ROW][CLM], int[][CLM], char *[], char *[]);
+void controlloMano  (char *[NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM]);
 
 // Mescola carte
 void shuffle(int wDeck[][13])
@@ -63,8 +65,8 @@ void deal(int wDeck[][13],
     int column;
 
     int r = 0;
-    // hand usato per catturare le prime 5 carte e usarle come manoAttuale nel gioco
-    int hand[10][2];
+    // hand usato per catturare le prime carte in base a quanti giocatori vi sono
+    int hand[NPLAYER * ROW][2];
 
     for (card = 1; card <= 52; card++)
     {
@@ -86,12 +88,12 @@ void deal(int wDeck[][13],
         }
     }
 
-    manoAttuale(wDeck, wFace, wSuit, hand);
+    //  manoAttuale(wDeck, wFace, wSuit, hand);
+
+    manoGiocatori(wDeck, wFace, wSuit, hand);
 
     return;
 }
-
-#
 
 void manoGiocatori(int wDeck[][13],
                    char *wFace[],
@@ -99,284 +101,196 @@ void manoGiocatori(int wDeck[][13],
                    int hand[][2])
 {
 
-    bool (*check[6])(char *manoAttuale[][2],
-                     int sortOrder[][2],
-                     int maxValore) = {poker, colore, scala, tris, doppiaCoppia, coppia};
-
     char *manoGiocatore[NPLAYER][ROW][CLM];
     int sortOrder[NPLAYER][ROW][CLM];
 
-/*
-    In caso di pareggio uso questa funzione per ricavarmi il giocatore con la carta 
-    più alta
- */
+    //  TODO:  In caso di pareggio uso questa funzione per ricavarmi il giocatore con la carta più alta
+    //  TODO:  Fare una funzione che controlli il pareggio
 
-    int cartaMaggioreMano[NPLAYER]; 
+    // Nel primo elemento vediamo cosa è uscito nel secondo la carta più alta in caso di parità
 
-/* 
-    Assegno alla mano del giocatore le carte e le coordinate delle carte estratte 
-    dal mazzo.
- */
-
-    int carteDaDistribuire = ROW * NPLAYER;
-
-    for(int tab = 0; tab < NPLAYER; tab++)
-    {
-        for(int riga = 0; riga < carteDaDistribuire; riga++)
-        {
-            manoGiocatore[tab][riga][1] = wFace[hand[riga][1]];
-            manoGiocatore[tab][riga][1] = wSuit[hand[riga][0]];
-        }
-    }
-
-    
-    /*  TODO: Creare una funzione sort con un array 3D
-        che riordini in un unico sort i mazzi dei giocatori.
-        l'idea è di farlo tramite un for non lo so da vedere.
-
-    */
-    /* Riordino le carte in mano in ordine crescente */
-//     sortMano(manoAttuale, wFace, sortOrder);
-
-    
-}
-    
-
-// TODO: Rifattorizzare il codice suddividere in 2 mani diverse. Rendere scalabile il gioco a più giocatori.
-void manoAttuale(int wDeck[][13],
-                 char *wFace[],
-                 char *wSuit[],
-                 int hand[][2])
-{
-
-    
-    bool (*check[6])(char *manoAttuale[][2],
-                     int sortOrder[][2],
-                     int maxValore) = {poker, colore, scala, tris, doppiaCoppia, coppia};
-
-
-    
     system("cls");
 
-    char *manoAttuale[5][2];
-    char *manoDue[5][2];
+    giveCardsCoord(manoGiocatore, hand, wFace, wSuit);
 
-    int sortOrder[5][2] = {0};
+    sortMani(manoGiocatore, wFace, sortOrder);
 
-    // Variabili usate per vedere quale è' la mano migliore
+    mostraMano(manoGiocatore, sortOrder);
 
-    int risultato_Mano_1 = 10;
-    int risultato_Mano_2 = 10;
-
-    /*    
-    La variabile maxValore serve a immagazzina il valore della carta che è coppia per confrontarla
-    con le altre mani in caso di parità
-
-    TODO: Inserire valore coppia dentro la matrice, creare una matrice [][][]
-
- */
-
-    int maxValore;
-
-    for (int x = 0; x < 5; x++)
-    {
-        manoAttuale[x][0] = wFace[hand[x][1]];
-        manoAttuale[x][1] = wSuit[hand[x][0]];
-    }
-
-    // Sorto la prima mando
-    sortMano(manoAttuale, wFace, sortOrder);
-
-    puts("\n### PRIMA MANO ### \n");
-
-    for (int x = 0; x < 5; x++)
-    {
-        int n = sortOrder[x][1];
-        printf("%s ", manoAttuale[n][0]);
-        printf("%s \n", manoAttuale[n][1]);
-    }
-
-    puts("\n");
-
-    for (int x = 0; x < 6; x++)
-    {
-        int key = 0;
-        if ((*check[x])(manoAttuale, sortOrder, maxValore))
-        {
-            switch (x)
-            {
-            case 0:
-                puts("POKER");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            case 1:
-                puts("COLORE");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            case 2:
-                puts("SCALA");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            case 3:
-                puts("TRIS");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            case 4:
-                puts("DOPPIA COPPIA");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            case 5:
-                puts("COPPIA");
-                key++;
-                risultato_Mano_1 = x;
-                break;
-
-            default:
-                puts("ERROR");
-                break;
-            }
-        }
-        if (key)
-            break;
-    }
-
-    // Do la seconda mano
-    for (int x = 0; x < 5; x++)
-    {
-        manoDue[x][0] = wFace[hand[5 + x][1]];
-        manoDue[x][1] = wSuit[hand[5 + x][0]];
-    }
-
-    // Sorto la seconda mano
-    sortMano(manoDue, wFace, sortOrder);
-
-    puts("\n### SECONDA MANO ### \n");
-
-    for (int x = 0; x < 5; x++)
-    {
-        int n = sortOrder[x][1];
-        printf("%s ", manoDue[n][0]);
-        printf("%s \n", manoDue[n][1]);
-    }
-
-    puts("\n");
-
-    for (int x = 0; x < 6; x++)
-    {
-        int key = 0;
-        if ((*check[x])(manoDue, sortOrder, maxValore))
-        {
-            switch (x)
-            {
-            case 0:
-                puts("POKER");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            case 1:
-                puts("COLORE");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            case 2:
-                puts("SCALA");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            case 3:
-                puts("TRIS");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            case 4:
-                puts("DOPPIA COPPIA");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            case 5:
-                puts("COPPIA");
-                key++;
-                risultato_Mano_2 = x;
-                break;
-
-            default:
-                puts("ERROR");
-                break;
-            }
-        }
-        if (key)
-            break;
-    }
-
-    // TODO: in caso di parità vedere chi la la mano più alta
-
-    puts("\n");
-
-    if (risultato_Mano_1 < risultato_Mano_2)
-    {
-        printf("MANO 1 HA VINTO\n");
-    }
-    else if (risultato_Mano_1 > risultato_Mano_2)
-    {
-        printf("MANO 2 HA VINTO\n");
-    }
-    else
-    {
-        printf("PARI\n");
-    }
+    controlloMano(manoGiocatore, sortOrder);
 
     return;
 }
 
-/*
- * sortMano funziona associando alla parola della carta il suo effettivo valore.
- */
-
-#define UGUALI 0
-void sortMano(char *manoAttuale[][2], char *wFace[], int sortOrder[][2])
+void controlloMano(char *manoGiocatore[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM])
 {
+    bool (*check[7])(char *manoGiocatore[NPLAYER][ROW][CLM],
+                     int sortOrder[NPLAYER][ROW][CLM],
+                     int idPlayer,
+                     int maxValore[]) = {poker, colore, scala, tris, doppiaCoppia, coppia, cartaAlta};
 
-    for (int cnt = 0; cnt < 5; cnt++)
+    int manoMaggiore[NPLAYER];
+
+    puts("\n\n");
+
+    int key = 0;
+    for(int tab = 0; tab < NPLAYER; tab++)
     {
-        for (int x = 0; x < 13; x++)
+        printf("La mano del giocatore %d  :", tab + 1);
+
+        for (int x = 0; x < 7; x++)
         {
-            if (strcmp(manoAttuale[cnt][0], wFace[x]) == UGUALI)
+            if ((*check[x])(manoGiocatore, sortOrder, tab, manoMaggiore))
             {
-                sortOrder[cnt][0] = x + 1;
-                sortOrder[cnt][1] = cnt;
+                switch (x)
+                {
+                case 0:
+                    puts("POKER");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+
+                case 1:
+                    puts("COLORE");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+
+                case 2:
+                    puts("SCALA");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+
+                case 3:
+                    puts("TRIS");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+
+                case 4:
+                    puts("DOPPIA COPPIA");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+
+                case 5:
+                    puts("COPPIA");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+                case 6:
+                    puts("CARTA ALTA");
+                    manoMaggiore[tab] = x;
+                    key++;
+                    break;
+                default:
+                    puts("ERROR");
+                    break;
+                }
+            }
+            if (key)
+                break;
+        }
+        key = 0;
+        puts("");
+    }
+
+    /* TODO: Implementare un sistema migliore di vincita
+    Il sistema deve rendersi conto di chi ha la carta maggiore tra gli n giocatori e 
+    stampare su terminale il vincitore */
+
+    if(manoMaggiore[0] < manoMaggiore[1])
+    {
+        if(manoMaggiore[0] < manoMaggiore[2])
+        {
+            puts("### IL GIOCATORE 1 HA VINTO ! ###");
+        }
+        else{
+             puts("### IL GIOCATORE 3 HA VINTO ! ###");
+        }
+    }
+    else
+    {
+        if(manoMaggiore[1] < manoMaggiore[2])
+        {
+            puts("### IL GIOCATORE 2 HA VINTO ! ###");
+        }
+        else{
+             puts("### IL GIOCATORE 3 HA VINTO ! ###");
+        }
+    }
+
+
+}
+
+//  Assegno alla mano del giocatore le carte e le coordinate delle carte estratte dal mazzo.
+void giveCardsCoord(char *manoGiocatore[NPLAYER][ROW][CLM], int hand[][CLM], char *wFace[], char *wSuit[])
+{
+    int carta = 0;
+
+    for (int tab = 0; tab < NPLAYER; tab++)
+    {
+        for (int riga = 0; riga < ROW; riga++, carta++)
+        {
+            manoGiocatore[tab][riga][0] = wFace[hand[carta][1]];
+            manoGiocatore[tab][riga][1] = wSuit[hand[carta][0]];
+        }
+    }
+}
+
+// Stampla le mani dei giocatori
+
+void mostraMano(char *manoGiocatore[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM])
+{
+    for (int tab = 0; tab < NPLAYER; tab++)
+    {
+        printf("\n \nMANO GIOCATORE %d\n\n", tab + 1);
+        for (int x = 0; x < ROW; x++)
+        {
+            int n = sortOrder[tab][x][1];
+            printf("%s ", manoGiocatore[tab][n][0]);
+            printf("%s \n", manoGiocatore[tab][n][1]);
+        }
+    }
+}
+
+void sortMani(char *manoGiocatori[NPLAYER][ROW][CLM], char *wFace[], int sortOrder[NPLAYER][ROW][CLM])
+{
+    for (int tab = 0; tab < NPLAYER; tab++)
+    {
+        for (int ncarte = 0; ncarte < ROW; ncarte++)
+        {
+            for (int x = 0; x < 13; x++)
+            {
+                if (strcmp(manoGiocatori[tab][ncarte][0], wFace[x]) == 0)
+                {
+                    sortOrder[tab][ncarte][0] = x + 1;
+                    sortOrder[tab][ncarte][1] = ncarte;
+                }
             }
         }
     }
 
-    for (int c = 0; c < 5 - 1; c++)
+    // Insertion Sort
+
+    for (int tab = 0; tab < NPLAYER; tab++)
     {
-        int s = c;
-        while (sortOrder[s][0] > sortOrder[s + 1][0])
+        for (int c = 0; c < ROW - 1; c++)
         {
-            for (int c = 0; c < 2; c++)
+            int s = c;
+            while (sortOrder[tab][s][0] > sortOrder[tab][s + 1][0])
             {
-                int key = sortOrder[s][c];
-                sortOrder[s][c] = sortOrder[s + 1][c];
-                sortOrder[s + 1][c] = key;
+                for (int c = 0; c < 2; c++)
+                {
+                    int key = sortOrder[tab][s][c];
+                    sortOrder[tab][s][c] = sortOrder[tab][s + 1][c];
+                    sortOrder[tab][s + 1][c] = key;
+                }
+                if (s == 0)
+                    break;
+                s--;
             }
-            if (s == 0)
-                break;
-            s--;
         }
     }
 
@@ -387,11 +301,13 @@ void sortMano(char *manoAttuale[][2], char *wFace[], int sortOrder[][2])
  * Scala controlla se la carta successiva -1 è uguale a quella precedente
  * se cosi non è allora non è scala
  */
-bool scala(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+
+
+bool scala(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
     for (int k = 0; k < 4; k++)
     {
-        if (sortOrder[k][0] != (sortOrder[k + 1][0] - 1))
+        if (sortOrder[idPlayer][k][0] != (sortOrder[idPlayer][k + 1][0] - 1))
         {
             return false;
         }
@@ -400,12 +316,12 @@ bool scala(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
     return true;
 }
 
-bool poker(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+bool poker(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
-    int carta_1 = sortOrder[0][0];
-    int carta_2 = sortOrder[1][0];
-    int carta_3 = sortOrder[2][0];
-    int carta_4 = sortOrder[3][0];
+    int carta_1 = sortOrder[idPlayer][0][0];
+    int carta_2 = sortOrder[idPlayer][1][0];
+    int carta_3 = sortOrder[idPlayer][2][0];
+    int carta_4 = sortOrder[idPlayer][3][0];
 
     if (carta_1 == carta_2 && carta_2 == carta_3 && carta_3 == carta_4)
     {
@@ -421,12 +337,12 @@ bool poker(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
  * i valori siano zero.
  * */
 
-bool colore(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+bool colore(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
-    int colore_1 = strcmp(manoAttuale[0][1], manoAttuale[1][1]);
-    int colore_2 = strcmp(manoAttuale[1][1], manoAttuale[2][1]);
-    int colore_3 = strcmp(manoAttuale[2][1], manoAttuale[3][1]);
-    int colore_4 = strcmp(manoAttuale[3][1], manoAttuale[4][1]);
+    int colore_1 = strcmp(manoGiocatori[idPlayer][0][1], manoGiocatori[idPlayer][1][1]);
+    int colore_2 = strcmp(manoGiocatori[idPlayer][1][1], manoGiocatori[idPlayer][2][1]);
+    int colore_3 = strcmp(manoGiocatori[idPlayer][2][1], manoGiocatori[idPlayer][3][1]);
+    int colore_4 = strcmp(manoGiocatori[idPlayer][3][1], manoGiocatori[idPlayer][4][1]);
 
     if (colore_1 == 0)
     {
@@ -436,12 +352,12 @@ bool colore(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
     return false;
 }
 
-bool tris(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+bool tris(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
 
-    int arr[5] = {sortOrder[0][0], sortOrder[1][0],
-                  sortOrder[2][0], sortOrder[3][0],
-                  sortOrder[4][0]};
+    int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
+                  sortOrder[idPlayer][2][0], sortOrder[idPlayer][3][0],
+                  sortOrder[idPlayer][4][0]};
 
     if (arr[0] == arr[1] && arr[1] == arr[2] && arr[3] != arr[4])
     {
@@ -461,11 +377,11 @@ bool tris(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
     return false;
 }
 
-bool doppiaCoppia(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+bool doppiaCoppia(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
-    int arr[5] = {sortOrder[0][0], sortOrder[1][0],
-                  sortOrder[2][0], sortOrder[3][0],
-                  sortOrder[4][0]};
+    int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
+                  sortOrder[idPlayer][2][0], sortOrder[idPlayer][3][0],
+                  sortOrder[idPlayer][4][0]};
 
     if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] == arr[2] && arr[3] == arr[4] && arr[1] != arr[4])
     {
@@ -485,11 +401,11 @@ bool doppiaCoppia(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
     return false;
 }
 
-bool coppia(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
+bool coppia(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
 {
-    int arr[5] = {sortOrder[0][0], sortOrder[1][0],
-                  sortOrder[2][0], sortOrder[3][0],
-                  sortOrder[4][0]};
+    int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
+                  sortOrder[idPlayer][2][0], sortOrder[idPlayer][3][0],
+                  sortOrder[idPlayer][4][0]};
 
     for (int f = 0; f < 4; f++)
     {
@@ -503,4 +419,9 @@ bool coppia(char *manoAttuale[][2], int sortOrder[][2], int maxValore)
     }
 
     return false;
+}
+
+bool cartaAlta(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+{
+    return true;
 }
