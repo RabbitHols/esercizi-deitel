05faba42e6da80a0294afb69c032177a1297d0a7
Robert Turturica <robertturturica@gmail.com>
2018-05-25 23:13:46 +0200

    Implementato controllo pareggio per la carta alta e la coppia

--- Esercizi/Capitolo 7/7.13/header.h
+++ Esercizi/Capitolo 7/7.13/header.h
@@ -5,7 +5,7 @@
  * @Last Modified time: 2018-05-22 22:50:34
  */
 
-// Funzioni
+// Funzione
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -18,13 +18,13 @@
 
 //prototipi
 
-bool doppiaCoppia   (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool poker          (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool colore         (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool tris           (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool scala          (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool coppia         (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
-bool cartaAlta      (char * [NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM], int , int [] );
+bool doppiaCoppia   (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool poker          (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool colore         (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool tris           (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool scala          (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool coppia         (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
+bool cartaAlta      (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM], int);
 
 void shuffle        (int[][13]);
 void deal           (int (*)[], char *[], char *[]);
@@ -33,7 +33,9 @@ void sortMani       (char * [NPLAYER][ROW][CLM], char *[], int[NPLAYER][ROW][CLM
 void mostraMano     (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM]);
 void manoGiocatori  (int (*)[], char *[], char *[], int[][2]);
 void giveCardsCoord (char * [NPLAYER][ROW][CLM], int[][CLM], char *[], char *[]);
-void controlloMano  (char *[NPLAYER][ROW][CLM], int [NPLAYER][ROW][CLM]);
+void controlloMano  (char * [NPLAYER][ROW][CLM], int[NPLAYER][ROW][CLM]);
+
+int cartaMaggiore   (int [NPLAYER][ROW][CLM], int, int, int);
 
 // Mescola carte
 void shuffle(int wDeck[][13])
@@ -66,7 +68,7 @@ void deal(int wDeck[][13],
 
     int r = 0;
     // hand usato per catturare le prime carte in base a quanti giocatori vi sono
-    int hand[NPLAYER * ROW][2];
+    int mazzoMischiato[NPLAYER * ROW][2];
 
     for (card = 1; card <= 52; card++)
     {
@@ -77,20 +79,15 @@ void deal(int wDeck[][13],
                 if (wDeck[row][column] == card)
                 {
                     // Catturo i risultati nell'array
-                    if (r < 52)
-                    {
-                        hand[r][0] = row;
-                        hand[r][1] = column;
-                        r++;
-                    }
+                    mazzoMischiato[r][0] = row;
+                    mazzoMischiato[r][1] = column;
+                    r++;
                 }
             }
         }
     }
 
-    //  manoAttuale(wDeck, wFace, wSuit, hand);
-
-    manoGiocatori(wDeck, wFace, wSuit, hand);
+    manoGiocatori(wDeck, wFace, wSuit, mazzoMischiato);
 
     return;
 }
@@ -103,11 +100,6 @@ void manoGiocatori(int wDeck[][13],
 
     char *manoGiocatore[NPLAYER][ROW][CLM];
     int sortOrder[NPLAYER][ROW][CLM];
-
-    //  TODO:  In caso di pareggio uso questa funzione per ricavarmi il giocatore con la carta più alta
-    //  TODO:  Fare una funzione che controlli il pareggio
-
-    // Nel primo elemento vediamo cosa è uscito nel secondo la carta più alta in caso di parità
 
     system("cls");
 
@@ -124,64 +116,70 @@ void manoGiocatori(int wDeck[][13],
 
 void controlloMano(char *manoGiocatore[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM])
 {
+
     bool (*check[7])(char *manoGiocatore[NPLAYER][ROW][CLM],
                      int sortOrder[NPLAYER][ROW][CLM],
-                     int idPlayer,
-                     int maxValore[]) = {poker, colore, scala, tris, doppiaCoppia, coppia, cartaAlta};
+                     int idPlayer) = {poker, colore, scala, tris, doppiaCoppia, coppia, cartaAlta};
 
-    int manoMaggiore[NPLAYER];
-
+    int combinazioneMaggiore[NPLAYER][2];
     puts("\n\n");
 
     int key = 0;
-    for(int tab = 0; tab < NPLAYER; tab++)
+    for (int tab = 0; tab < NPLAYER; tab++)
     {
         printf("La mano del giocatore %d  :", tab + 1);
 
         for (int x = 0; x < 7; x++)
         {
-            if ((*check[x])(manoGiocatore, sortOrder, tab, manoMaggiore))
+            if ((*check[x])(manoGiocatore, sortOrder, tab))
             {
                 switch (x)
                 {
                 case 0:
                     puts("POKER");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
 
                 case 1:
                     puts("COLORE");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
 
                 case 2:
                     puts("SCALA");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
 
                 case 3:
                     puts("TRIS");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
 
                 case 4:
                     puts("DOPPIA COPPIA");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
 
                 case 5:
                     puts("COPPIA");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
                 case 6:
                     puts("CARTA ALTA");
-                    manoMaggiore[tab] = x;
+                    combinazioneMaggiore[tab][0] = x;
+                    combinazioneMaggiore[tab][1] = tab;
                     key++;
                     break;
                 default:
@@ -196,32 +194,68 @@ void controlloMano(char *manoGiocatore[NPLAYER][ROW][CLM], int sortOrder[NPLAYER
         puts("");
     }
 
-    /* TODO: Implementare un sistema migliore di vincita
-    Il sistema deve rendersi conto di chi ha la carta maggiore tra gli n giocatori e 
-    stampare su terminale il vincitore */
+    //TESTING: Implementazione incompleta, leggere la descrizione sotto per capire cosa si sta facendo
 
-    if(manoMaggiore[0] < manoMaggiore[1])
+    //TODO: Scelta vincitore
+    /*  La scelta vincitore va gestita in questa maniera, vince chi ha la variabile più bassa,
+        essa viene assegnata durante il controllo del true delle funzione in ordine di importanza
+        dunque la più bassa ha il valore più alto.
+        In ogni funzione implemento un sort personalizzato delle carte in modo da ordinarle in base
+        all'importanza della mano 
+        ES: COPPIA:
+            le carte che rendono la mano coppia le metto per prime e poi ordino le altre in ordine
+            di grandezza (kicker).
+
+        In caso la mano fosse uguale devo implementare anche un assegnazione del valore dei colori
+        dal più importante al meno
+    */
+
+    int vincitore[1][2];
+    vincitore[0][0] = combinazioneMaggiore[0][0];
+    vincitore[0][1] = combinazioneMaggiore[0][1];
+
+    for(int n = 1; n < NPLAYER; n++)
     {
-        if(manoMaggiore[0] < manoMaggiore[2])
+        if( vincitore[0][0] > combinazioneMaggiore[n][0] )
         {
-            puts("### IL GIOCATORE 1 HA VINTO ! ###");
+            vincitore[0][0] = combinazioneMaggiore[n][0];
+            vincitore[0][1] = combinazioneMaggiore[n][1];
+            continue;
         }
-        else{
-             puts("### IL GIOCATORE 3 HA VINTO ! ###");
-        }
+
+         if( vincitore[0][0] == combinazioneMaggiore[n][0] && ( vincitore[0][0] == 6 || vincitore[0][0] == 5  ) )
+         {
+             int winner = cartaMaggiore(sortOrder, vincitore[0][1], n, 0);
+             if(winner == combinazioneMaggiore[n][1])
+             {
+                 vincitore[0][0] = combinazioneMaggiore[n][0];
+                 vincitore[0][1] = combinazioneMaggiore[n][1];
+             }
+
+         }
+
     }
-    else
+
+    printf("Il vincitore e' il giocatore nr %d", vincitore[0][1] + 1);
+
+}
+
+int cartaMaggiore(int sortOrder[NPLAYER][ROW][CLM],
+                  int idPlayer_1, 
+                  int idPlayer_2,
+                  int cont)
+{
+    if(cont == 4)
     {
-        if(manoMaggiore[1] < manoMaggiore[2])
-        {
-            puts("### IL GIOCATORE 2 HA VINTO ! ###");
-        }
-        else{
-             puts("### IL GIOCATORE 3 HA VINTO ! ###");
-        }
+        return (sortOrder[idPlayer_1][cont][0] > sortOrder[idPlayer_2][cont][0]) ? idPlayer_1 : idPlayer_2; 
     }
 
+    if(sortOrder[idPlayer_1][cont][0] == sortOrder[idPlayer_2][cont][0])
+    {
+        return cartaMaggiore(sortOrder, idPlayer_1, idPlayer_2, cont + 1);
+    }
 
+    return (sortOrder[idPlayer_1][cont][0] > sortOrder[idPlayer_2][cont][0]) ? idPlayer_1 : idPlayer_2;
 }
 
 //  Assegno alla mano del giocatore le carte e le coordinate delle carte estratte dal mazzo.
@@ -302,8 +336,7 @@ void sortMani(char *manoGiocatori[NPLAYER][ROW][CLM], char *wFace[], int sortOrd
  * se cosi non è allora non è scala
  */
 
-
-bool scala(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool scala(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
     for (int k = 0; k < 4; k++)
     {
@@ -316,7 +349,7 @@ bool scala(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][
     return true;
 }
 
-bool poker(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool poker(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
     int carta_1 = sortOrder[idPlayer][0][0];
     int carta_2 = sortOrder[idPlayer][1][0];
@@ -337,22 +370,22 @@ bool poker(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][
  * i valori siano zero.
  * */
 
-bool colore(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool colore(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
     int colore_1 = strcmp(manoGiocatori[idPlayer][0][1], manoGiocatori[idPlayer][1][1]);
     int colore_2 = strcmp(manoGiocatori[idPlayer][1][1], manoGiocatori[idPlayer][2][1]);
     int colore_3 = strcmp(manoGiocatori[idPlayer][2][1], manoGiocatori[idPlayer][3][1]);
     int colore_4 = strcmp(manoGiocatori[idPlayer][3][1], manoGiocatori[idPlayer][4][1]);
 
-    if (colore_1 == 0)
+    if (!colore_1 && !colore_2 && !colore_3 && !colore_4)
     {
-        return colore_1 && colore_2 == 0 && colore_3 == 0 && colore_4 == 0;
+        return true;
     }
 
     return false;
 }
 
-bool tris(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool tris(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
 
     int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
@@ -373,15 +406,15 @@ bool tris(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][C
     {
         return true;
     }
-
     return false;
 }
 
-bool doppiaCoppia(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool doppiaCoppia(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
     int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
                   sortOrder[idPlayer][2][0], sortOrder[idPlayer][3][0],
                   sortOrder[idPlayer][4][0]};
+
 
     if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] == arr[2] && arr[3] == arr[4] && arr[1] != arr[4])
     {
@@ -401,27 +434,89 @@ bool doppiaCoppia(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER
     return false;
 }
 
-bool coppia(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool coppia(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
-    int arr[5] = {sortOrder[idPlayer][0][0], sortOrder[idPlayer][1][0],
-                  sortOrder[idPlayer][2][0], sortOrder[idPlayer][3][0],
-                  sortOrder[idPlayer][4][0]};
+
+    int temp_1;
+    int temp_2;
 
     for (int f = 0; f < 4; f++)
     {
         for (int c = f + 1; c < 5; c++)
         {
-            if (arr[f] == arr[c])
-            {
+            if (sortOrder[idPlayer][f][0] == sortOrder[idPlayer][c][0])
+            {           
+    /*
+                Se trova una coppia sorta la coppia ai primi 2 posti e il restante dei kicker in ordine decrescente              
+    */
+
+                int arr[5][2] = { -1 };
+
+                arr[0][0] = sortOrder[idPlayer][f][0];
+                arr[0][1] = sortOrder[idPlayer][f][1];
+
+                arr[1][0] = sortOrder[idPlayer][c][0];
+                arr[1][1] = sortOrder[idPlayer][c][1];
+
+                for(int n = 2, k = 0; n < 5; n++, k++)
+                {
+                    if(sortOrder[idPlayer][k][0] == arr[0][0])
+                    {   k++;
+                        n--;
+                        continue;
+                    }
+
+                    arr[n][0] = sortOrder[idPlayer][k][0];
+                    arr[n][1] = sortOrder[idPlayer][k][1];
+                }
+
+                temp_1 = arr[2][0];
+                arr[2][0] = arr[4][0];
+                arr[4][0] = temp_1;  
+
+                
+                temp_2 = arr[2][1];
+                arr[2][1] = arr[4][1];
+                arr[4][1] = temp_2;  
+            
+                for(int n = 0; n < ROW; n++)
+                {
+                    sortOrder[idPlayer][n][0] = arr[n][0];
+                    sortOrder[idPlayer][n][1] = arr[n][1];
+                }
+
                 return true;
             }
         }
     }
 
+    
     return false;
 }
 
-bool cartaAlta(char * manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer, int maxValore[])
+bool cartaAlta(char *manoGiocatori[NPLAYER][ROW][CLM], int sortOrder[NPLAYER][ROW][CLM], int idPlayer)
 {
+
+    int temp_1;
+    int temp_2;
+
+    int n = 0;
+    int k = 4;
+    while(n != 2)
+    {   
+
+        temp_1 = sortOrder[idPlayer][n][0];
+        temp_2 = sortOrder[idPlayer][n][1];
+
+        sortOrder[idPlayer][n][0] = sortOrder[idPlayer][k][0];
+        sortOrder[idPlayer][n][1] = sortOrder[idPlayer][k][1];
+
+        sortOrder[idPlayer][k][0] = temp_1;
+        sortOrder[idPlayer][k][1] = temp_2;
+
+        k--;
+        n++;
+    }
+
     return true;
 }
